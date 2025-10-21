944e979085f52192a6b0d4a15995c27bbcbbc60f
nishimind <159146332+nishimind@users.noreply.github.com>
2025-10-21 19:21:42 +0900

    当たり判定を分離しました　当たり判定で主人公が少し隠れてしまう

---
+++ Assets/!Script/Player&Enemy/CollisionBase.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBase : MonoBehaviour
+{
+    public HealthBase health;
+    protected virtual void Start() { health = GetComponent<HealthBase>(); }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       TakeDamage(collision);
+    }
+    protected virtual void TakeDamage(Collider2D collision)
+    {
+        var bullet = collision.GetComponent<BulletDamage>();
+        if (bullet != null)
+        {
+            // É_ÉÅÅ[ÉWèàóù
+
+            if (health != null)
+                health.TakeDamage(bullet.damage);
+
+            // íeÇè¡Ç∑
+            if (bullet.destroyOnHit)
+            {
+                var checker = collision.GetComponent<CameraChecker>();
+                if (checker != null && checker._pool != null)
+                {
+                    checker._pool.Release(collision.gameObject);
+                }
+                else
+                {
+                    Destroy(collision.gameObject);
+                }
+            }
+        }
+    }
+}
--- Assets/!Script/Player&Enemy/PlayerCollision.cs
+++ Assets/!Script/Player&Enemy/PlayerCollision.cs
@@ -1,31 +1,8 @@
 using UnityEngine;
 
-public class PlayerCollision : MonoBehaviour
-{public HealthBase health;
-    void Start() { health = GetComponent<HealthBase>(); }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        var bullet = collision.GetComponent<BulletDamage>();
-        if (bullet != null)
-        {
-            // É_ÉÅÅ[ÉWèàóù
-           
-            if (health != null)
-                health.TakeDamage(bullet.damage);
-
-            // íeÇè¡Ç∑
-            if (bullet.destroyOnHit)
-            {
-                var checker = collision.GetComponent<CameraChecker>();
-                if (checker != null && checker._pool != null)
-                {
-                    checker._pool.Release(collision.gameObject);
-                }
-                else
-                {
-                    Destroy(collision.gameObject);
-                }
-            }
-        }
-    }
+public class PlayerCollision : CollisionBase
+{
+   
+    protected virtual void Start() { }
+   
 }
