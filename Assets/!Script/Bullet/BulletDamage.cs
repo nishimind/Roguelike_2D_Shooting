29bc4ec8e465d0599250d6ae6e9e93c54ea01c2f
nishimind <159146332+nishimind@users.noreply.github.com>
2025-11-07 22:26:28 +0900

    凡ミスしてました、すみません

--- Assets/!Script/Bullet/BulletDamage.cs
+++ Assets/!Script/Bullet/BulletDamage.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BulletDamage : MonoBehaviour
 {
-   [] public int damage = 1; // ’e‚ÌˆĞ—Í
+   [HideInInspector] public int damage = 1; // ’e‚ÌˆĞ—Í
     public bool destroyOnHit = true; // “–‚½‚Á‚½‚çÁ‚¦‚é‚©‚Ç‚¤‚©
     public bool grazed=false;
     public bool onlyGraze = false;
