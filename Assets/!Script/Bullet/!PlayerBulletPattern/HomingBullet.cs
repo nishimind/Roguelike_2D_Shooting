284930ba1ff05b944b6bbb9e07a00cc10c1acc9b
nishimind <159146332+nishimind@users.noreply.github.com>
2026-02-10 21:04:07 +0900

    ホーミングがめっちゃ出るのを修正

--- Assets/!Script/Bullet/!PlayerBulletPattern/HomingBullet.cs
+++ Assets/!Script/Bullet/!PlayerBulletPattern/HomingBullet.cs
@@ -1,21 +1,21 @@
 ﻿using UnityEngine;
 using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
-public class HomingBullet : MonoBehaviour
+public class HomingBullet : BulletBase
 {
-    [SerializeField] private float _speed = 5f;
+   
     [SerializeField] private float _rotateSpeed = 200f;
     [SerializeField] private float _maxRotateAngle = 30f; // 最大回転角度（度）
-    private Rigidbody2D _rb;
+   
     private Transform _target;
 
-    void Awake()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-    }
 
-    async void Start()
+
+    protected override void Initialize()
     {
+        base.Initialize();
+    
+    
         // 発射時点で最も近いEnemyタグのターゲットを探す
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
         if (enemies.Length > 0)
@@ -31,13 +31,16 @@ public class HomingBullet : MonoBehaviour
                 }
             }
         }
-       await UniTask.Delay(3000).ContinueWith(() =>
+     /*  await UniTask.Delay(3000).ContinueWith(() =>
         {
             // 10秒後に弾を消す
             _target = null;
-        });
+        });*/
     }
+    protected virtual void Update()
+    {
 
+    }
     void FixedUpdate()
     {
         if (_rb == null) return;
